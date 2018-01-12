7ed65b5666d6dec07b80c31faf04b0c26af1a011
Jschles1 <jschles1@ramapo.edu>
2018-01-11 22:06:19 -0500

    Created readline interface, declared function expressions for call, welcome, getSystem, & listBestsellers

--- app.js
+++ app.js
@@ -2,3 +2,33 @@ const readline = require('readline');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+/**************************************************************************************/
+
+var call = () => {
+  welcome();
+  getSystem();
+};
+
+var welcome = () => {
+
+};
+
+var getSystem = () => {
+  // Get input on system to scrape:
+  listBestsellers(system);
+};
+
+var listBestsellers = (system) => {
+
+};
+
+
+
+
+
