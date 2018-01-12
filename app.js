1a6bdfc3a0b712132e5d112c2ef9f0ea6dc59178
Jschles1 <jschles1@ramapo.edu>
2018-01-11 22:27:55 -0500

    Completed welcome, & getSystem, changed var's to const's

--- app.js
+++ app.js
@@ -7,26 +7,30 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
-/**************************************************************************************/
-
-var call = () => {
+const call = () => {
   welcome();
-  getSystem();
 };
 
-var welcome = () => {
-
+const welcome = () => {
+  console.log("Welcome to the Best Selling Games App!");
+  setTimeout(() => {
+    getSystem();
+  }, 1000);
 };
 
-var getSystem = () => {
+const getSystem = () => {
   // Get input on system to scrape:
-  listBestsellers(system);
+  rl.question("Which system's current bestselling games would you like to see? (Xbox One, PS4, Switch, Wii U, 3DS): ", (system) => {
+    listBestsellers(input);
+  });
 };
 
-var listBestsellers = (system) => {
-
+const listBestsellers = (input) => {
+  const input = input.toLowerCase();
+  rl.close();
 };
+
+call();
 
 
 
